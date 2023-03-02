e7aed287242531df5d7ddb28309b4f4975747708
unknown <rosairekota@gmail.com>
2023-03-02 13:09:33 +0100

    feat:add handlebarsAdapter

--- src/modules/mailer/mailer.module.ts
+++ src/modules/mailer/mailer.module.ts
@@ -1,4 +1,23 @@
+import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 
-@Module({})
-export class MailerModule {}
+@Module({
+  imports: [
+    NestMailerModule.forRoot({
+      transport: {},
+      defaults: {},
+      
+      // config  handlebarsAdapter
+      template: {
+        dir: join(__dirname, 'template'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+})
+export class MailModule {}
